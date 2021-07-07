0d5f42acd66ca416494b046f0fc098751116f2cb
Naseemudheen E <naseemudheene@gmail.com>
2021-07-07 09:25:21 +0530

    wsgi updated

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from src.app import app
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    app = app()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
