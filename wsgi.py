ee5e6be08ed2b1fd6b09d1b3519933f6ffee378c
Naseemudheen E <naseemudheene@gmail.com>
2021-07-07 09:21:35 +0530

    added wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+from src.app import app
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
