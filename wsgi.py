393b322e17cb130400f052ca26548c4f18eb08c6
Naseemudheen E <naseemudheene@gmail.com>
2021-07-07 09:31:28 +0530

    wsgi updated 2

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from src.app import app
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app = app()
+    app.run(threaded=True, port=5000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
